ca95e1a3c65be03bb95f1958b96d7cb4d59aac7a
Chucks Nwosu <chucksn611@gmail.com>
2023-01-16 23:30:41 -0600

    movie-app v1.0 responsive modal

--- src/components/castCard.jsx
+++ src/components/castCard.jsx
@@ -1,17 +1,27 @@
-function CastCard({ castImgPath, castName, character }) {
+import { TooltipVisibilityContext } from "./modal";
+import { useContext } from "react";
+
+const CastCard = ({ castImgPath, castName, character, onClick, index }) => {
+  const castCardContext = useContext(TooltipVisibilityContext);
+  const tooltipVisibility = castCardContext.tooltipVisibility;
+  const clickedCard = castCardContext.clickedCard;
+
   let castImg =
     castImgPath === null
       ? "./not_available.jpg"
       : `https://image.tmdb.org/t/p/w185${castImgPath}`;
   return (
-    <div className="cast-profile">
+    <div className="cast-profile" onClick={onClick}>
       <img src={castImg} alt="profile" className="cast-image" />
-      <span className="cast-name">
-        {castName} <br />
-        <span className="xter">'{character}'</span>
-      </span>
+      <span className="cast-name">{castName}</span>
+      {clickedCard === index && tooltipVisibility && (
+        <span className="character-tooltip">
+          <span className="character-txt">Character:</span>
+          {character}
+        </span>
+      )}
     </div>
   );
-}
+};
 
 export default CastCard;
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,23 +1,11 @@
-import {
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalBody,
-  ModalCloseButton,
-  Box,
-  Flex,
-  Image,
-  AspectRatio,
-  Button,
-} from "@chakra-ui/react";
+import { useRef, useState, createContext } from "react";
 import { useDispatch } from "react-redux";
-import { AiFillCaretRight } from "react-icons/ai";
 import CastCard from "./castCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import { Autoplay, Navigation } from "swiper";
-import { useRef } from "react";
+
+export const TooltipVisibilityContext = createContext();
 
 function MovieDetailModal({
   modalPosterPath,
@@ -27,13 +15,41 @@ function MovieDetailModal({
   castData,
   year,
 }) {
-  const { onOpen, onClose } = useDisclosure();
   const dispatch = useDispatch();
+  const modal = useRef();
 
-  const handleClose = () => {
-    onClose();
+  const [tooltipVisibility, setTooltipVisibility] = useState(false);
+  const [clickedCard, setClickedCard] = useState(null);
+
+  // When the modal is shown, we want a fixed body
+  document.body.style.overflow = "hidden";
+
+  // When the modal is hidden...
+  const restoreScroll = () => {
+    document.body.style.overflow = "unset";
+  };
+
+  const onClose = () => {
+    modal.current.style.display = "none";
     dispatch({ type: "CARD_CLICK_RESET" });
     dispatch({ type: "MODAL_DATA_RESET" });
+    restoreScroll();
+  };
+
+  window.onclick = (event) => {
+    if (event.target === modal.current) {
+      modal.current.style.display = "none";
+      dispatch({ type: "CARD_CLICK_RESET" });
+      dispatch({ type: "MODAL_DATA_RESET" });
+      restoreScroll();
+    }
+  };
+
+  document.onkeydown = (event) => {
+    if (event.key === "Escape") modal.current.style.display = "none";
+    dispatch({ type: "CARD_CLICK_RESET" });
+    dispatch({ type: "MODAL_DATA_RESET" });
+    restoreScroll();
   };
 
   let image =
@@ -41,132 +57,85 @@ function MovieDetailModal({
       ? "./no-poster.jpg"
       : `https://image.tmdb.org/t/p/w500${modalPosterPath}`;
 
-  return (
-    <Modal
-      isOpen={onOpen}
-      onClose={handleClose}
-      size={{ base: "xl", md: "2xl", lg: "4xl", xl: "6xl" }}
-      scrollBehavior="inside"
-    >
-      <ModalOverlay bgColor="blackAlpha.200" />
-      <ModalContent
-        pt="0.5rem"
-        pb="0.5rem"
-        bgColor="gray.800"
-        h={{ base: "100vh", sm: "640px" }}
-      >
-        <ModalCloseButton color="gray.300" />
-        <ModalBody className="modal-body">
-          <Flex
-            flexWrap={{ base: "wrap", lg: "nowrap" }}
-            justify={{ base: "center", lg: "normal" }}
-          >
-            <Image
-              src={image}
-              alt="poster"
-              maxW={{ base: "60%", sm: "70%", md: "80%", lg: "400px" }}
-              objectFit="contain"
-            />
-            <Flex direction="column" justify="space-around" w="100%" pl="1rem">
-              <Box textAlign="center" w="100%" p="0.6rem">
-                <Box
-                  as="h1"
-                  fontSize={{
-                    base: "1.2rem",
-                    md: "1.5rem",
-                    lg: "2rem",
-                    xl: "2.2rem",
-                  }}
-                  fontWeight="500"
-                  fontFamily='"Ubuntu", sans-serif'
-                  color="teal.400"
-                >
-                  {movieTitle}
-                </Box>
-                <Box
-                  as="span"
-                  fontSize={{
-                    base: "0.9rem",
-                    lg: "1rem",
-                    xl: "1.2rem",
-                  }}
-                  fontWeight="500"
-                  fontFamily="cursive"
-                  color="yellow.400"
-                >
-                  {tagline}
-                  <br />
-                  <span style={{ color: "green", fontSize: "1.2rem" }}>
-                    {year}
-                  </span>
-                </Box>
-              </Box>
-              <Box
-                as="span"
-                bgColor="gray.900"
-                color="gray.200"
-                p="0.6rem"
-                borderRadius="0.9rem"
-                fontSize={{ base: "0.9rem", lg: "1rem" }}
-                fontWeight="500"
-                overflowY="scroll"
-                maxHeight={{ base: "8rem", sm: "11rem" }}
-                className="overview"
-              >
-                {overview}
-              </Box>
-              <Box
-                display={{ base: "none", sm: "flex" }}
-                maxW={{ base: "100%", lg: "28rem", xl: "43rem" }}
-                mt="0.9rem"
-                position="relative"
-                alignItems="center"
-                maxH="15rem"
-              >
-                <i className="fa-solid fa-circle-chevron-left"></i>
-                <Swiper
-                  spaceBetween={0}
-                  modules={[Navigation, Autoplay]}
-                  navigation={{
-                    nextEl: ".fa-circle-chevron-right",
-                    prevEl: ".fa-circle-chevron-left",
-                  }}
-                  breakpoints={{
-                    500: { slidesPerView: 5 },
-                    280: { slidesPerView: 4 },
-                  }}
-                  autoplay={{
-                    delay: 2000,
-                    disableOnInteraction: true,
-                  }}
-                >
-                  {castData.map((data) => (
-                    <SwiperSlide>
-                      <CastCard
-                        key={data.cast_id}
-                        castName={data.name}
-                        character={data.character}
-                        castImgPath={data.profile_path}
-                      />
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
+  const handleCastCardClick = (index) => {
+    setTooltipVisibility(!tooltipVisibility);
+    setClickedCard(index);
+  };
 
-                <i className="fa-solid fa-circle-chevron-right"></i>
-              </Box>
-              <Button
-                colorScheme="red"
-                mt="0.8rem"
-                w="100%"
-                leftIcon={<AiFillCaretRight />}
-              >
-                WATCH TRAILER
-              </Button>
-            </Flex>
-          </Flex>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+  return (
+    <div ref={modal} className="modal">
+      <div className="modal-content">
+        <span onClick={onClose} className="close-icon">
+          &times;
+        </span>
+        <div className="modal-body">
+          <div className="details-img-ctn">
+            <div className="img-ctn">
+              <img src={image} alt="poster" className="modal-poster" />
+            </div>
+            <div className="details-ctn">
+              <div className="title-date-ctn">
+                <span className="modal-title">{movieTitle}</span>
+                <span className="tagline">{tagline}</span>
+                <span className="modal-date">{year}</span>
+              </div>
+
+              <div className="overview">
+                <span className="overview.txt">{overview}</span>
+              </div>
+
+              <div className="cast-ctn">
+                <span className="cast-txt">Cast</span>
+
+                <div className="outer-swiper-ctn">
+                  <i className="fa-solid fa-circle-chevron-left"></i>
+                  <Swiper
+                    spaceBetween={0}
+                    modules={[Navigation, Autoplay]}
+                    navigation={{
+                      nextEl: ".fa-circle-chevron-right",
+                      prevEl: ".fa-circle-chevron-left",
+                    }}
+                    breakpoints={{
+                      2300: { slidesPerView: 7 },
+                      1900: { slidesPerView: 6 },
+                      1024: { slidesPerView: 5 },
+                      280: { slidesPerView: 4 },
+                    }}
+                    autoplay={{
+                      delay: 800,
+                      disableOnInteraction: true,
+                    }}
+                  >
+                    {castData.map((data, index) => (
+                      <SwiperSlide>
+                        <TooltipVisibilityContext.Provider
+                          value={{ tooltipVisibility, clickedCard }}
+                        >
+                          <CastCard
+                            onClick={() => handleCastCardClick(index)}
+                            key={data.cast_id}
+                            index={index}
+                            castName={data.name}
+                            character={data.character}
+                            castImgPath={data.profile_path}
+                          />
+                        </TooltipVisibilityContext.Provider>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+
+                  <i className="fa-solid fa-circle-chevron-right"></i>
+                </div>
+              </div>
+              <button className="play-trailer-btn">
+                <i class="fa-brands fa-youtube"></i> PLAY TRAILER
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
