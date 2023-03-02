a88fb2c156f4a22a555a31bad47b93f6d282d3da
Chucks Nwosu <chucksn611@gmail.com>
2023-03-01 21:42:16 -0600

    loader anination added to main-slide

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -168,6 +168,7 @@ function Home() {
             <span className="tv-header">Now Playing</span>
             <div className="home-slideshow">
               <div className="main-slide-ctn">
+                {!nowPlayingList && <img src={tailSpinLoader} alt="loading" />}
                 {nowPlayingList && (
                   <>
                     <i
