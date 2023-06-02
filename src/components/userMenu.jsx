65ff6da0355fde3c0441356448a94bc42523ec88
Chucks Nwosu <chucksn611@gmail.com>
2023-06-01 21:45:56 -0500

    user-menu position set to fixed

--- src/components/userMenu.jsx
+++ src/components/userMenu.jsx
@@ -78,7 +78,7 @@ function UserMenu() {
       {isLogged && userMenuToggle && (
         <div
           ref={userMenuRef}
-          className="user-menu-layer absolute top-0 left-0 w-full h-full z-50"
+          className="user-menu-layer fixed top-0 left-0 w-full h-full z-50"
         >
           <div className="user-menu-content min-h-72 min-w-60 text-black/80 bg-slate-300 border border-gray-300 rounded-lg absolute top-16 right-4 flex flex-col p-4 justify-between">
             <div className="user-profile p-2 flex flex-col justify-center items-center">
